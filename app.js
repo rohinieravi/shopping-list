90dd0095527408b22b59371f4db45b36a36d8416
Rohini Ravindranath <rohinieravi@yahoo.com>
2017-04-20 00:08:28 -0700

    review fixes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var state = {
 
 // State modification functions
 var addItem = function(state, item) {
-	alert("here");
 	var itemObj = {
 		itemName: item,
 		checked: false
@@ -13,28 +12,35 @@ var addItem = function(state, item) {
 };
 
 var toggleItem = function(state, item) {
-	var index = state.items.map(function(val) { return val.itemName;}).indexOf(item);
+	var index = getItemIndex(state, item);
 	if(index > -1) {
 		state.items[index].checked = !state.items[index].checked;
 	}
 };
 
 var deleteItem = function(state, item) {
-	var index = state.items.map(function(val) { return val.itemName;}).indexOf(item)
+	var index = getItemIndex(state, item);
 	if(index > -1) {
 		state.items.splice(index, 1);
 	}
 };
 
+var getItemIndex = function(state, item) {
+	var items = state.items.map(function(val) { return val.itemName;});
+	return items.indexOf(item);
+};
+
 // Render functions
 var renderList = function(state, element) {
-	var shoppingControls = '<div class="shopping-item-controls">'+
-		'<button class="shopping-item-toggle">'+
-		'<span class="button-label">check</span>'+
-		'</button>'+
-		'<button class="shopping-item-delete">'+
-		'<span class="button-label">delete</span>'+
-		'</button></div>';
+	var shoppingControls = 
+	'<div class="shopping-item-controls">'+
+	'	<button class="shopping-item-toggle">'+
+	'		<span class="button-label">check</span>'+
+	'	</button>'+
+	'	<button class="shopping-item-delete">'+
+	'		<span class="button-label">delete</span>'+
+	'	</button>'+
+	'</div>';
     var itemsHTML = state.items.map(function(item) {
         var newElem = $('<li><span class="shopping-item">' + item.itemName + '</span>' + shoppingControls + '</li>');
         newElem.find(".shopping-item").toggleClass('shopping-item__checked',item.checked);
